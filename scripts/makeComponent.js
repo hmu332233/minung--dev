7cebd0ad474afbb59a4158eb2c63b2d587d71137
minung.han <hmu332233@gmail.com>
2018-05-18 01:14:01 +0900

    [Modify] script

--- scripts/makeComponent.js
+++ scripts/makeComponent.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 const componentName = args[0];
-const componentPath = `../src/components/${componentName}`;
+const componentPath = `${__dirname}/../src/components/${componentName}`;
 
-const templateJsxData = fs.readFileSync('./template/Component.jsx', 'utf8');
-const templateIndexData = fs.readFileSync('./template/index.js', 'utf8');
+const templateJsxData = fs.readFileSync(`${__dirname}/template/Component.jsx`, 'utf8');
+const templateIndexData = fs.readFileSync(`${__dirname}/template/index.js`, 'utf8');
 
 const jsxData = templateJsxData.replace(/\[COMPONENT_NAME\]/gi, componentName);
 const indexData = templateIndexData.replace(/\[COMPONENT_NAME\]/gi, componentName);
@@ -14,4 +14,3 @@ fs.mkdirSync(componentPath);
 fs.writeFileSync(`${componentPath}/${componentName}.jsx`, jsxData);
 fs.writeFileSync(`${componentPath}/${componentName}.scss`, '');
 fs.writeFileSync(`${componentPath}/index.js`, indexData);
-
