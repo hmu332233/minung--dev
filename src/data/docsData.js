e441e348aad2c7dc09f8e1f60da840e74cb8582c
minung.han <hmu332233@gmail.com>
2018-06-04 04:27:39 +0900

    [Modify] remove dummy docs data / [Add] docsData module for Docs

---
+++ src/data/docsData.js
@@ -0,0 +1,37 @@
+const pagination = {
+  name: 'pagination',
+  description: '배열과 페이지당 사이즈, 페이지 번호를 받아 페이지에 해당하는 배열만큼을 리턴한다',
+  def: `function paginate (array, page_size, page_number) {
+  return array.slice(page_number * page_size, (page_number + 1) * page_size);
+}`,
+  runFunctions: [
+    'paginate([1, 2, 3, 4, 5, 6], 2, 1); // [3, 4]',
+    'paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4, 0); // [1, 2, 3, 4]'
+  ]
+};
+
+const truncateLongText = {
+  name: 'truncateLongText',
+  description: '텍스트와 최대 길이, 대체 텍스트를 받아 최대 길이를 넘는 문자에 대해서 대체 텍스트로 변환한다',
+  def: `function truncateLongText(_text, _maxLength, _lastText) {
+  var text = _text;
+	var maxLength = _maxLength || 20;
+	var lastText = _lastText || "...";
+
+	if (text.length > maxLength) {
+		text = text.substr(0, maxLength) + lastText;
+	}
+	return text;
+};`,
+  runFunctions: [
+    'truncateLongText("가나다라마바사", 3); // 가나다...',
+    'truncateLongText("가나다라마바사", 5, "###"); // 가나다라마###'
+  ]
+};
+
+const docsData = [
+  pagination,
+  truncateLongText
+];
+
+export default docsData;
--- src/pages/Docs/Docs.jsx
+++ src/pages/Docs/Docs.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Docs.scss';
 import { Menus, DocsViewer } from 'components';
 import { SideNav } from 'containers';
+import docsData from 'data/docsData';
 
 class Docs extends React.Component {
   constructor(props) {
@@ -31,7 +32,7 @@ class Docs extends React.Component {
         <Menus />
         <div className={styles.Docs}>
           <SideNav
-            items={docsItems}
+            items={this.props.docsItems}
             itemClickHandler={itemName => {
               this.changeShownDocItem(itemName);
             }}
@@ -45,48 +46,11 @@ class Docs extends React.Component {
   }
 }
 
-
-const pagination = {
-  name: 'pagination',
-  description: '배열과 페이지당 사이즈, 페이지 번호를 받아 페이지에 해당하는 배열만큼을 리턴한다',
-  def: `function paginate (array, page_size, page_number) {
-  return array.slice(page_number * page_size, (page_number + 1) * page_size);
-}`,
-  runFunctions: [
-    'paginate([1, 2, 3, 4, 5, 6], 2, 1); // [3, 4]',
-    'paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4, 0); // [1, 2, 3, 4]'
-  ]
-};
-
-const truncateLongText = {
-  name: 'truncateLongText',
-  description: '텍스트와 최대 길이, 대체 텍스트를 받아 최대 길이를 넘는 문자에 대해서 대체 텍스트로 변환한다',
-  def: `function truncateLongText(_text, _maxLength, _lastText) {
-  var text = _text;
-	var maxLength = _maxLength || 20;
-	var lastText = _lastText || "...";
-
-	if (text.length > maxLength) {
-		text = text.substr(0, maxLength) + lastText;
-	}
-	return text;
-};`,
-  runFunctions: [
-    'truncateLongText("가나다라마바사", 3); // 가나다...',
-    'truncateLongText("가나다라마바사", 5, "###"); // 가나다라마###'
-  ]
-};
-
-const docsItems = [
-  pagination,
-  truncateLongText
-];
-
 Docs.propTypes = {
   docsItems: PropTypes.array
 };
 Docs.defaultProps = {
-  docsItems
+  docsItems: docsData
 };
 
 export default Docs;
